e30ba208fab5aca66556af040bdf83f4a09c4194
Annemhony <148298131+Annemhony@users.noreply.github.com>
2025-09-10 16:48:35 +0800

    RouteInput.js

--- components/RouteInput.js
+++ components/RouteInput.js
@@ -8,7 +8,6 @@ const RouteInput = ({
   onEndChange,
 }) => {
   return (
-    // A container is added to manage the position of the line image
     <View style={styles.container}>
       <TextInput
         style={styles.input}
@@ -18,14 +17,12 @@ const RouteInput = ({
         onChangeText={onStartChange}
       />
       <TextInput
-        // A top margin is added to create space between the inputs
         style={[styles.input, { marginTop: 15 }]}
         placeholder="Ending Location"
         placeholderTextColor="#8e8e93"
         value={endValue}
         onChangeText={onEndChange}
       />
-      {/* The Image is now positioned absolutely within the container */}
       <Image
         source={require('../assets/line.png')}
         style={styles.lineConnector}
@@ -36,7 +33,7 @@ const RouteInput = ({
 
 const styles = StyleSheet.create({
   container: {
-    position: 'relative', // Ensures the absolute positioning is relative to this container
+    position: 'relative', 
   },
   input: {
     backgroundColor: '#f0f0f0',
@@ -44,15 +41,12 @@ const styles = StyleSheet.create({
     padding: 15,
     fontSize: 16,
   },
-  // The style for the image is updated to position it correctly
   lineConnector: {
     height: 20,
     width: 20,
     resizeMode: 'contain',
     position: 'absolute',
-    // This value is calculated to perfectly center the image in the gap
     top: 47.5,
-    // This property will now center the image horizontally
     alignSelf: 'center',
   },
 });
